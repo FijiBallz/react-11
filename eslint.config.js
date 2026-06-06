4c56e985f0b86c7ec485dba77be280e15ab5cabc
FijiBallz <filipdobromilski2@gmail.com>
2026-06-06 13:45:13 +0200

    zadanie11

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+        <p>Aktualna wartość licznika: {count}</p>
+        <button onClick={() => setCount(count - 1)}>-1</button>
+        <button onClick={() => setCount(count + 1)}>+1</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
